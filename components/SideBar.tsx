cc7d9aea41f1bd2f4e55d0f89e97ffd9e6bed86d
alfonsoj-entwickler <77501021+alfonsoj-entwickler@users.noreply.github.com>
2023-07-07 17:26:13 +0200

    [TASK] create sidebar component

---
+++ components/SideBar.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+const SideBar = () => {
+    return (
+        <>
+            <Image width={300} height={100} src="/assets/images/vercel.svg" alt="image logo" />
+        </>
+    )
+}
+
+export default SideBar;
