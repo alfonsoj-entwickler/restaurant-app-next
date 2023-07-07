d24667af0887280721847738d77208d365824048
alfonsoj-entwickler <77501021+alfonsoj-entwickler@users.noreply.github.com>
2023-07-07 17:27:23 +0200

    [TASK] create prisma data

---
+++ prisma/data/categories.ts
@@ -0,0 +1,30 @@
+const categories = [
+    {
+        icono: "coffee",
+        nombre: "Coffee"
+      },
+      {
+        icono: "burger",
+        nombre: "Burgers"
+      },
+      {
+        icono: "pizza",
+        nombre: "Pizzas"
+      },
+      {
+        icono: "dona",
+        nombre: "Donas"
+      },
+      {
+        icono: "cake",
+        nombre: "Cakes"
+      },
+      {
+        icono: "cookie",
+        nombre: "Cookies"
+      }
+]
+
+export {
+    categories 
+}
---
+++ prisma/data/products.ts
@@ -0,0 +1,10 @@
+
+const products = [
+  {
+    
+  }
+]
+
+export {
+    products 
+}
