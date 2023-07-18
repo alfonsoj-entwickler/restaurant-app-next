fc0c19831428fb97448fa4f0362bb7878838de18
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2023-07-18 19:14:34 +0200

    [TASK] reset project

--- hooks/useRestaurant.tsx
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import RestaurantContext from "@/context/RestaurantProvider";
-
-const useRestaurant = () => {
-    return useContext(RestaurantContext);
-}
-
-export default useRestaurant;
