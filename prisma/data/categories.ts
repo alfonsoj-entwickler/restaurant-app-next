79ea15ec8e8feb0ffa8d2e153a31e6fa69a68603
alfonsoj-entwickler <alfonsoj.entwickler@gmail.com>
2023-07-19 08:40:03 +0200

    [TASK] create schema and DB Mysql with prisma

---
+++ prisma/data/categories.ts
@@ -0,0 +1,28 @@
+const categories = [
+  {
+    name: "Café",
+    icon: "cafe",
+  },
+  {
+    name: "Hamburguesas",
+    icon: "hamburguesa",
+  },
+  {
+    name: "Pizzas",
+    icon: "pizza",
+  },
+  {
+    name: "Donas",
+    icon: "dona",
+  },
+  {
+    name: "Pasteles",
+    icon: "pastel",
+  },
+  {
+    name: "Galletas",
+    icon: "galletas",
+  },
+];
+
+export { categories };
